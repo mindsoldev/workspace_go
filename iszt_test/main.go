67de29ff926d222af26a9898c3f935eca9fab99c
Harsan Peter <pharsan@yahoo.com>
2023-01-26 16:01:42 +0100

    Before cleaning

--- iszt_test/main.go
+++ iszt_test/main.go
@@ -7,37 +7,41 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
 
 const startDateMarker string = "<startDate>"
 const endDateMarker string = "<endDate>"
+const dailyresultMarker string = "<dailyresult>"
 
 type forecastApi struct {
-	ForecastURL           *string
-	LastTenDaysURL        *string
-	HistoricalURLTemplate *string
-	latitude              *string
-	longitude             *string
-	refDate               *time.Time
+	ForecastURL                *string
+	LastTenDaysURL             *string
+	HistoricalURLTemplate      *string
+	MinusPlusOneDayURLTemplate *string
+	latitude                   *string
+	longitude                  *string
+	refDate                    *time.Time
+	dailyResultParts           *[]string
+	timeNames                  *[]string
 }
 
-/*
-	type dataEntry struct {
-		dataLabel string
-		value     float32
-	}
+type dataEntry struct {
+	DataLabel string
+	Value     float64
+}
 
-	type dayEntry struct {
-		dayLabel    string
-		dataEntries []dataEntry
-	}
+type dayEntry struct {
+	DayLabel    string
+	DataEntries []dataEntry
+}
 
-	type forecast struct {
-		dayEntrys []dayEntry
-	}
-*/
+type forecastEntry struct {
+	DayEntries []dayEntry
+}
+
 func main() {
 	forecastApi := new(forecastApi)
 	emptyString := ""
@@ -45,21 +49,32 @@ func main() {
 	forecastApi.LastTenDaysURL = &emptyString
 	forecastApi.HistoricalURLTemplate = &emptyString
 	forecastApi.HistoricalURLTemplate = &emptyString
+	forecastApi.MinusPlusOneDayURLTemplate = &emptyString
 	lattitude := "47.497913"
 	forecastApi.latitude = &lattitude
 	longitude := "19.040236"
 	forecastApi.longitude = &longitude
 	now := time.Now()
 	forecastApi.refDate = &now
+	//dailyResultParts := []string{"temperature_2m_min", "temperature_2m_max"}
+	dailyResultParts := []string{"temperature_2m_max"}
+	forecastApi.dailyResultParts = &dailyResultParts
+	forecastApi.timeNames = &[]string{"past", "now", "future"}
 
-	createForecastURL(*forecastApi)
-	printResult(getCurrentForecast(*forecastApi))
-	createLastTenDaysURL(*forecastApi)
-	printResult(getLasttTenDaysForecast(*forecastApi))
-	createHistoricalURLTemplate(*forecastApi)
-	printResult(getHistoricalForecast(*forecastApi, "2023-01-10", "2023-01-12"))
+	/*
+		createForecastURL(*forecastApi)
+		printResult(getCurrentForecast(*forecastApi))
+		createLastTenDaysURL(*forecastApi)
+		printResult(getLasttTenDaysForecast(*forecastApi))
+		createHistoricalURLTemplate(*forecastApi)
+		printResult(getHistoricalForecast(*forecastApi, "2023-01-10", "2023-01-12"))
+	*/
+	createMinusPlusOneDayURLTemplate(*forecastApi)
+	printResult(getMinusPlusOneDayForecast(*forecastApi))
+	os.Exit(1)
 }
 
+/*
 func createForecastURL(forecastApi forecastApi) {
 	forecastURL := "https://api.open-meteo.com/v1/forecast"
 	forecastURL += "?" + "latitude=" + *forecastApi.latitude + "&longitude=" + *forecastApi.longitude
@@ -135,12 +150,69 @@ func getHistoricalForecast(forecastApi forecastApi, startDate string, endDate st
 
 	return hasError, "Historical forecast", formattedBodyBytes
 }
-
-/*
-func getMinusPlusOneDayForecast() {
-
-}
 */
+
+func createMinusPlusOneDayURLTemplate(forecastApi forecastApi) {
+	// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max&timezone=Europe%2FBerlin&start_date=2023-01-23&end_date=2023-01-25
+	historicalURLTemplate := "https://api.open-meteo.com/v1/forecast"
+	historicalURLTemplate += "?" + "latitude=" + *forecastApi.latitude + "&longitude=" + *forecastApi.longitude
+	historicalURLTemplate += "&timezone=Europe/Budapest"
+	historicalURLTemplate += "&start_date=" + startDateMarker
+	historicalURLTemplate += "&end_date=" + endDateMarker
+	historicalURLTemplate += "&daily=" + dailyresultMarker
+	*forecastApi.HistoricalURLTemplate = historicalURLTemplate
+}
+
+func getMinusPlusOneDayForecast(forecastApi forecastApi) (bool, string, []byte) {
+	requestURL := *forecastApi.MinusPlusOneDayURLTemplate
+	startDate := forecastApi.refDate.AddDate(0, 0, -1)
+	endDate := forecastApi.refDate.AddDate(0, 0, 1)
+	requestURL = strings.Replace(requestURL, startDateMarker, startDate.Format("2006-01-02"), 1)
+	requestURL = strings.Replace(requestURL, endDateMarker, endDate.Format("2006-01-02"), 1)
+	dailyResultTemplate := createDailyResultTemplate(*forecastApi.dailyResultParts)
+	requestURL = strings.Replace(requestURL, dailyresultMarker, dailyResultTemplate, 1)
+	log.Println("Minus-plus one day forecast URL: " + requestURL)
+
+	response := executeQuery(requestURL)
+	defer response.Body.Close()
+
+	bodyBytes := readBody(response)
+
+	formattedBodyBytes := formatJson(bodyBytes)
+
+	hasError := hasErrorInResponseBody(formattedBodyBytes)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(formattedBodyBytes), &result)
+
+	daily := result["daily"]
+
+	var dataItems []dataEntry
+	// for i := 0; i < len(*forecastApi.dailyResultParts); i++ {
+	for _, partTitle := range *forecastApi.dailyResultParts {
+		dataItems = append(dataItems, createDataItem(daily, partTitle, forecastApi))
+	}
+
+	var dayItems []dayEntry
+	for i := 0; i < 3; i++ {
+		dayItem := dayEntry{
+			DayLabel:    (*forecastApi.timeNames)[i],
+			DataEntries: dataItems,
+		}
+		dayItems = append(dayItems, dayItem)
+	}
+
+	forecast := forecastEntry{
+		DayEntries: dayItems,
+	}
+	fmt.Println(forecast)
+	js, err := json.Marshal(forecast)
+	fmt.Println(js, err)
+	jsf := formatJson(js)
+	//fmt.Println(jsf)
+
+	return hasError, "Minus-plus one day forecast:", jsf
+}
 
 func executeQuery(requestURL string) *http.Response {
 	response, err := http.Get(requestURL)
@@ -182,4 +254,28 @@ func printResult(hasError bool, message string, result []byte) {
 		errorMessage = " error"
 	}
 	fmt.Printf("\n%s%s:\n%s\n\n", message, errorMessage, result)
+}
+
+func createDailyResultTemplate(dailyResultParts []string) string {
+	var dailyResultTemplate string
+	for _, template := range dailyResultParts {
+		if len(dailyResultTemplate) > 0 {
+			dailyResultTemplate += ","
+		}
+		dailyResultTemplate += template
+	}
+	return dailyResultTemplate
+}
+
+func createDataItem(daily interface{}, partName string, forecastApi forecastApi) dataEntry {
+
+	temperatures := daily.(map[string]interface{})[partName]
+	temperature0 := temperatures.([]interface{})[0]
+
+	dataItem := dataEntry{
+		DataLabel: (*forecastApi.dailyResultParts)[0],
+		Value:     temperature0.(float64),
+	}
+
+	return dataItem
 }
